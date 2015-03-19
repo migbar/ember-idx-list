78affa5d56c08abaeb59890098de9ba4cc3565f4
Miguel Barcos <miguel@galar.com>
2015-03-19 14:23:35 -0400

    Do not assign to imported Config

--- app/initializers/em-idx-list.js
+++ app/initializers/em-idx-list.js
@@ -1,11 +1,11 @@
 import Em from 'ember';
-import Config from 'ember-idx-utils/config'
+import IdxConfig from 'ember-idx-utils/config'
 
 export default {
   name: 'ember-idx-list',
   initialize: function() {
-    if (!Em.Config) {
-        Em.Config = Config = Config.create()
+    if (!Em.IdxConfig) {
+        Em.IdxConfig = IdxConfig.create()
     }
   }
 };
